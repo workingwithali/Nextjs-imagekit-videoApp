758e25f217aafc0181a49db62b7a62cff813ff27
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-06-17 11:21:27 +0500

    update

--- lib/db.ts
+++ lib/db.ts
@@ -23,7 +23,7 @@ export async function connectToDatabase() {
       maxPoolSize: 10,
     };
 
-    mongoose.connect(MONGODB_URI, opts).then(() => mongoose.connection);
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then(() => mongoose.connection);
   }
 
   try {
