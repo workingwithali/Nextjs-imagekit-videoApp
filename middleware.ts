75015e5721a05a2c854d755b76b048d150d03cc3
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-06-23 13:27:20 +0500

    update

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware() {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized({ req, token }) {
+        const { pathname } = req.nextUrl;
+        if (
+          pathname.startsWith("/api/auth") ||
+          pathname === "/login" ||
+          pathname === "/register"
+        )
+          return true;
+
+        if (pathname === "/" || pathname.startsWith("/api/videos")) {
+          return true;
+        }
+
+        return !!token;
+      },
+    },
+  }
+);
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    "/((?!_next/static|_next/image|favicon.ico|public/).*)",
+  ],
+};
