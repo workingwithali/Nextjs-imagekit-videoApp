41b92ea0cdf1870ec25129badd529110444a311a
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-06-23 14:42:57 +0500

    update

--- app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-'use'
-export default function Footer() {
-    return (
-        <footer className="w-full bg-black text-center p-4 border-t border-purple-600 text-purple-500">
-            © {new Date().getFullYear()} MyVideoApp. All rights reserved.
-        </footer>
-    );
-}
-  
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,17 @@
-import Link from 'next/link';
+
 import Header from './components/Header';
-import Navbar from '@/app/components/Navbar'
-import Footer from '@/app/components/Footer'
+
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-black text-purple-500">
-      <Navbar />
+      
 
-      {/* Main Body */}
-      <main className="flex-grow flex flex-col items-center justify-center text-center p-8">
-        <h1 className="text-4xl font-bold mb-6">Welcome to the Video Uploader</h1>
-        
-        <Link href="/upload">
-          <button className="px-6 py-3 bg-purple-600 text-white rounded-lg hover:bg-purple-700 transition">
-            Upload Video
-          </button>
-        </Link>
-      </main>
+      
+      
+      {/* <Header /> */}
 
-      <Footer />
+      
     </div>
   );
 }
