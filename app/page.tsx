5781a5ca611cb147016b466c01c716499fcc7958
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-06-28 11:50:03 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,16 @@
 "use client";
-import { useEffect, useState } from "react";
-import VideoFeed from "./components/VideoFeed";
 import Header from "./components/Header";
-import { apiClient } from "@/lib/api-client";
-import { IVideo } from "@/models/Video";
 import VideoFeedWrapper from "./components/VideoFeedWrapper";
 
 export default function HomePage() {
-  // const [videos, setVideos] = useState<IVideo[]>([]);
 
-  // useEffect(() => {
-  //   apiClient.getVideos().then((res) => {
-      
-  //     const data = res as { videos: IVideo[] };
-  //     setVideos(data.videos);
-      
-
-  //   });
-    
-  // }, []);
+ 
 
   return (
     <>
       <Header />
       <VideoFeedWrapper />
-      {/* {videos ? (
-        <VideoFeed videos={videos} />
-      ) : (
-        <p className="text-center">Loading videos...</p>
-      )} */}
+     
     </>
   );
 }
