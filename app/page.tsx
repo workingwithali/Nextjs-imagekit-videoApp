a03c80599ec2a0b41ac9abfa14b3505392e529aa
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-06-28 09:34:15 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 
 import Header from './components/Header';
-
+import VideoFeed from './components/VideoFeed';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-black text-purple-500">
       
       <Header />
-      {/* <VideoFeed /> */}
+      <VideoFeed videos={[]} />
+
       
       
       
